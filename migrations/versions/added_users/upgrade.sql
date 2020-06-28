ad68e3cb6a19a50ed792d424f472cf2bb23b4d8c
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-06-28 19:37:15 +0200

    changed migration to reflect users class

--- migrations/versions/added_users/upgrade.sql
+++ migrations/versions/added_users/upgrade.sql
@@ -1,8 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
+    identifier UUID DEFAULT uuid_generate_v4(),
     email VARCHAR(256) UNIQUE NOT NULL,
     name VARCHAR(256) NOT NULL,
+    admin BOOLEAN DEFAULT False,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (email)
+    PRIMARY KEY (identifier)
 );
 CREATE INDEX users_email_idx ON users USING btree (email);
 CREATE INDEX users_name_idx ON users USING btree (name);
