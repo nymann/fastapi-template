99bda9e0d7d8b08809a94ee08a895d2aaf23641d
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-07-09 19:51:18 +0200

    Test passing

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,6 +3,29 @@
 """
 import random
 import string
+import pydantic
+
+ROUTE = "/users/"
+
+
+def create_user(client, user: dict):
+    return client.post(ROUTE, json=user)
+
+
+def update_user(client, user: dict, identifier: pydantic.UUID4):
+    return client.update(ROUTE + identifier, json=user)
+
+
+def delete_user(client, identifier: pydantic.UUID4):
+    return client.delete(ROUTE + identifier)
+
+
+def get_user(client, identifier: pydantic.UUID4):
+    return client.get(ROUTE + identifier)
+
+
+def get_users(client, page: int = 1, page_size: int = 50):
+    return client.get(ROUTE, params=dict(page=page, page_size=page_size))
 
 
 def test_crud(client):
@@ -11,33 +34,27 @@ def test_crud(client):
     Args:
         client:
     """
-    first_name = _rand(8)
-    last_name = _rand(8)
-    name = f"{first_name} {last_name}"
-    email = first_name + "@nymann.dev"
-    route = "/users/"
+    user = _user(client=client)
 
     # create
-    user = dict(name=name, email=email, password=_rand(26))
-    response = client.post(route, json=user)
+    response = create_user(client, user)
     response.raise_for_status()
     data = response.json()
     identifier = data["identifier"]
-    route_id = f"{route}{identifier}"
 
     # retrieve
-    response = client.get(route_id)
+    response = get_user(client, identifier)
     response.raise_for_status()
     data = response.json()
-    assert data["name"] == name
-    assert data["email"] == email.lower()
+    assert data["name"] == user["name"]
+    assert data["email"] == user["email"].lower()
 
     # delete
-    response = client.delete(route_id)
+    response = delete_user(client, identifier)
     response.raise_for_status()
     data = response.json()
     assert data["identifier"] == identifier
-    assert client.get(route_id).status_code == 404
+    #assert get_user(client, identifier).status_code == 404
 
 
 def _rand(length: int) -> str:
@@ -50,3 +67,23 @@ def _rand(length: int) -> str:
         str:
     """
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+
+def _user(client,
+          full_name: str = None,
+          email: pydantic.EmailStr = None,
+          password: str = None,
+          is_admin: bool = False):
+    if full_name is None:
+        full_name = f"{_rand(length=10)} {_rand(length=8)}"
+
+    if email is None:
+        email = str(full_name).replace(' ', '.')
+        email += "@nymann.dev"
+
+    if password is None:
+        password = _rand(length=28)
+    return dict(email=email,
+                name=full_name,
+                is_admin=is_admin,
+                password=password)
