35e298eaa26d2ddc688f04623db1751a713109c4
Kristian Jakobsen <kristian@nymann.dev>
2023-04-30 01:05:05 +0200

    Use docker buildx for multi images and add versioning

--- {{cookiecutter.repo_name}}/make/ci.mk
+++ {{cookiecutter.repo_name}}/make/ci.mk
@@ -1,18 +1,4 @@
-DOCKER_REPO?={%- if cookiecutter.docker_registry != "default" -%}{{cookiecutter.docker_registry}}/{% endif %}{{ cookiecutter.repo_name }}
-DOCKER_TAG?=${DOCKER_REPO}:$(shell git describe --tag --always | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-
 package: ${VERSION} setup.py
 	@python setup.py sdist bdist_wheel
 
-docker-build: ${VERSION}
-	@docker build -f docker/Dockerfile .
-
-docker-push: ${VERSION}
-	@docker build \
-        --cache-from ${DOCKER_REPO}:latest \
-	    -t ${DOCKER_REPO}:latest \
-	    -t ${DOCKER_TAG} \
-		-f docker/Dockerfile .
-	@docker push --all-tags ${DOCKER_REPO}
-
-.PHONY:docker-build docker-push package
+.PHONY:package
